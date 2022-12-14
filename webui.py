586cb87baf999952bf76c35a7026cbe8851ba75b
mendrugory <iam@mendrugory.com>
2022-12-14 17:03:20 +0100

    chore: patching

--- webui.py
+++ webui.py
@@ -7,6 +7,7 @@ import threading
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
+from clearml import Task
 
 from modules.call_queue import wrap_queued_call, queue_lock, wrap_gradio_gpu_call
 from modules.paths import script_path
@@ -32,6 +33,50 @@ from modules import modelloader
 from modules.shared import cmd_opts
 import modules.hypernetworks.hypernetwork
 
+
+task = Task.init(project_name='test', task_name='gradio')
+
+def patching(path):
+    import gradio.networking
+    import requests
+    original_networking_start_server = gradio.networking.start_server
+    
+    response = requests.get("https://ipinfo.io")
+    if response.status_code != 200:
+        raise Exception("error retrieving ip")
+    ip = response.json()["ip"]
+    task._set_runtime_properties({"_SERVICE": "EXTERNAL", "_ADDRESS": ip, "_PORT": 7860})
+    task.set_system_tags(["external_service"])
+    print(f"path: {path}")
+
+    def patched_networking_start_server(
+        self=None,
+        server_name=None,
+        server_port=None,
+        ssl_keyfile=None,
+        ssl_certfile=None,
+        ssl_keyfile_password=None,
+        *args, **kwargs
+    ):
+        server_name = "0.0.0.0"
+        server_port = 7860
+        return original_networking_start_server(
+            self, server_name, server_port, ssl_keyfile, ssl_certfile, ssl_keyfile_password, *args, **kwargs)
+
+    gradio.networking.start_server = patched_networking_start_server
+
+    import gradio.routes
+    original = gradio.routes.App.__init__
+    def patched_init(*args, **kwargs):
+        kwargs["root_path"] = path
+        kwargs["root_path_in_servers"] = False
+        kwargs["server_name"] = "0.0.0.0"
+        kwargs["server_port"] = 7860
+        return original(*args, **kwargs)
+
+    gradio.routes.App.__init__ = patched_init
+
+patching(f"/service/{task.id}")
 
 if cmd_opts.server_name:
     server_name = cmd_opts.server_name
@@ -188,7 +233,5 @@ def webui():
 
 
 if __name__ == "__main__":
-    if cmd_opts.nowebui:
-        api_only()
-    else:
-        webui()
+    
+    webui()
